fdb34d7e6fe3b12ff881a7bfafd342860b960f77
flfer <florian.ferrier@tutamail.com>
2022-08-24 14:30:05 +0200

    Clean CMakeList and remove setting of RN4871_LOG_ENABLE from driver

--- logs.c
+++ logs.c
@@ -10,32 +10,37 @@ char logBuffer[LOG_BUFFER_LEN_MAX+1] = "";
 void logger(enum log_level_e level, const char *message, ...) {
     assert(NULL != message);
 
-    if(!(RN4871_LOG_ENABLE & level))
+    #ifdef RN4871_LOG_ENABLE
+        if(!(RN4871_LOG_ENABLE & level)) {
+            return;
+        }
+
+        va_list args;
+        va_start(args, message);
+
+        int logBufferLen = 0;
+
+        switch(level) {
+            case LOG_ERROR:
+                logBufferLen = snprintf(logBuffer, LOG_BUFFER_LEN_MAX, "[ERROR] ");
+                break;
+            case LOG_DEBUG:
+                logBufferLen = snprintf(logBuffer, LOG_BUFFER_LEN_MAX, "[DEBUG] ");
+                break;
+            case LOG_INFO:
+                logBufferLen = snprintf(logBuffer, LOG_BUFFER_LEN_MAX, "[INFO] ");
+                break;
+            default:
+                break;
+        }
+
+        vsnprintf(&logBuffer[logBufferLen], LOG_BUFFER_LEN_MAX - logBufferLen, message, args);
+
+        logBufferLen = (int)strlen(logBuffer);
+        rn4871LogSenderAPI(logBuffer, logBufferLen);
+
+        va_end(args);
+    #else
         return;
-
-    va_list args;
-    va_start(args, message);
-
-    int logBufferLen = 0;
-
-    switch(level) {
-        case LOG_ERROR:
-            logBufferLen = snprintf(logBuffer, LOG_BUFFER_LEN_MAX, "[ERROR] ");
-            break;
-        case LOG_DEBUG:
-            logBufferLen = snprintf(logBuffer, LOG_BUFFER_LEN_MAX, "[DEBUG] ");
-            break;
-        case LOG_INFO:
-            logBufferLen = snprintf(logBuffer, LOG_BUFFER_LEN_MAX, "[INFO] ");
-            break;
-        default:
-            break;
-    }
-
-    vsnprintf(&logBuffer[logBufferLen], LOG_BUFFER_LEN_MAX - logBufferLen, message, args);
-
-    logBufferLen = (int)strlen(logBuffer);
-    rn4871LogSenderAPI(logBuffer, logBufferLen);
-
-    va_end(args);
+    #endif
 }
